a0631f2e11081226630cff5a47fde8fa7de3db22
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2016-11-10 10:33:20 +0100

    Implement flip action

--- src/org/parosproxy/paros/extension/filter/imageFilterActions/Flip.java
+++ src/org/parosproxy/paros/extension/filter/imageFilterActions/Flip.java
@@ -1,11 +1,26 @@
 package org.parosproxy.paros.extension.filter.imageFilterActions;
 
+import org.apache.commons.io.IOUtils;
 import org.parosproxy.paros.network.HttpMessage;
+
+import java.io.IOException;
 
 public class Flip extends ImageFilterAction {
 
     @Override
     public void onHttpResponseReceive(HttpMessage msg) {
-        // TODO: Implement this
+        if (msg.getResponseHeader().isEmpty() || msg.getResponseHeader().isImage() || msg.getResponseBody().length() == 0) {
+            return;
+        }
+
+        try {
+            String css = IOUtils.toString(getClass().getResourceAsStream("/resource/oss/flipimages.css"));
+            String result = msg.getResponseBody().toString().replaceAll("<head>",
+                    "<head>\n<style media=\"screen\" type=\"text/css\">\n" + css + "</style>");
+            msg.getResponseBody().setBody(result);
+            msg.getResponseHeader().setContentLength(msg.getResponseBody().length());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
