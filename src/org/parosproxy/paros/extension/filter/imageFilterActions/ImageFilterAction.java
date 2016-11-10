b71c9666a8c71eb471d1c9b47b3004500d93bba8
Anthony Clays <anthonyclays@gmail.com>
2016-11-11 00:06:39 +0100

    Added imageFromBytes and bytesFromImage to abstract superclass ImageFilterAction

--- src/org/parosproxy/paros/extension/filter/imageFilterActions/ImageFilterAction.java
+++ src/org/parosproxy/paros/extension/filter/imageFilterActions/ImageFilterAction.java
@@ -1,5 +1,13 @@
 package org.parosproxy.paros.extension.filter.imageFilterActions;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
 import org.parosproxy.paros.network.HttpMessage;
 
 public abstract class ImageFilterAction {
@@ -16,4 +24,18 @@ public abstract class ImageFilterAction {
 
 
     public abstract void onHttpResponseReceive(HttpMessage msg);
+
+    static BufferedImage imageFromBytes(byte[] bytes) throws IOException {
+        InputStream in = new ByteArrayInputStream(bytes);
+        return ImageIO.read(in);
+    }
+
+    static byte[] bytesFromImage(BufferedImage image) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", baos);
+        baos.flush();
+        byte[] bytes = baos.toByteArray();
+        baos.close();
+        return bytes;
+    }
 }
