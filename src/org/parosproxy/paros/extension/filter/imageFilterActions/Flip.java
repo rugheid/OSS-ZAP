d89ab96d8c7a09619c643ffa0b9c3ad90c8170c6
Anthony Clays <anthonyclays@gmail.com>
2016-11-10 12:17:48 +0100

    Implemented Flip without relying on CSS

--- src/org/parosproxy/paros/extension/filter/imageFilterActions/Flip.java
+++ src/org/parosproxy/paros/extension/filter/imageFilterActions/Flip.java
@@ -1,26 +1,51 @@
 package org.parosproxy.paros.extension.filter.imageFilterActions;
 
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.parosproxy.paros.network.HttpMessage;
 
-import java.io.IOException;
-
 public class Flip extends ImageFilterAction {
+	
+	public static BufferedImage flipImage(BufferedImage img) {
+        // https://examples.javacodegeeks.com/desktop-java/awt/image/flipping-a-buffered-image/
+        AffineTransform tx = AffineTransform.getScaleInstance(1, -1);
+        tx.translate(0, -img.getHeight(null));
+        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+        return op.filter(img, null);
+	}
 
     @Override
     public void onHttpResponseReceive(HttpMessage msg) {
-        if (msg.getResponseHeader().isEmpty() || msg.getResponseHeader().isImage() || msg.getResponseBody().length() == 0) {
+        if (msg.getResponseHeader().isEmpty() || !msg.getResponseHeader().isImage() || msg.getResponseBody().length() == 0) {
             return;
         }
-
+        
         try {
-            String css = IOUtils.toString(getClass().getResourceAsStream("/resource/oss/flipimages.css"));
-            String result = msg.getResponseBody().toString().replaceAll("<head>",
-                    "<head>\n<style media=\"screen\" type=\"text/css\">\n" + css + "</style>");
-            msg.getResponseBody().setBody(result);
-            msg.getResponseHeader().setContentLength(msg.getResponseBody().length());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            byte[] byteImage = msg.getResponseBody().getBytes();
+            InputStream in = new ByteArrayInputStream(byteImage);
+            BufferedImage bufferedImage = ImageIO.read(in);
+            if (bufferedImage == null) return;
+            BufferedImage resultImage = Flip.flipImage(bufferedImage);            
+           
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(resultImage, "png", baos);
+            baos.flush();
+            byte[] resultBytes = baos.toByteArray();
+			baos.close();
+	        msg.setResponseBody(resultBytes);
+	        msg.getResponseHeader().setContentLength(resultBytes.length);
+		} catch (IOException e) {
+        	Logger.getLogger(Flip.class).error(e.getMessage(), e);
+		}
     }
 }
