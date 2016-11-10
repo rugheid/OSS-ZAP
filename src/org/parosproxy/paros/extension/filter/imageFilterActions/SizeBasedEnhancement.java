1f66a90d146c630f548f06981b9fcc9864749569
Anthony Clays <anthonyclays@gmail.com>
2016-11-11 00:09:13 +0100

    minor refactoring in SizeBasedEnhancement

--- src/org/parosproxy/paros/extension/filter/imageFilterActions/SizeBasedEnhancement.java
+++ src/org/parosproxy/paros/extension/filter/imageFilterActions/SizeBasedEnhancement.java
@@ -1,68 +1,53 @@
 package org.parosproxy.paros.extension.filter.imageFilterActions;
 
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-
-import javax.imageio.ImageIO;
 
 import org.apache.log4j.Logger;
-import org.parosproxy.paros.extension.filter.FilterImages;
 import org.parosproxy.paros.network.HttpMessage;
-import org.parosproxy.paros.network.HttpResponseHeader;
 
 public class SizeBasedEnhancement extends ImageFilterAction {
 
-	public static BufferedImage toGrayscale(BufferedImage img) {
-		 //get image width and height
-	    int width = img.getWidth();
-	    int height = img.getHeight();
+    private static BufferedImage toGrayscale(BufferedImage img) {
+        // get image width and height
+        int width = img.getWidth();
+        int height = img.getHeight();
 
-	    //convert to grayscale
-	    for(int y = 0; y < height; y++){
-	      for(int x = 0; x < width; x++){
-	        int p = img.getRGB(x,y);
+        // convert to grayscale
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                int p = img.getRGB(x,y);
 
-	        int a = (p>>24)&0xff;
-	        int r = (p>>16)&0xff;
-	        int g = (p>>8)&0xff;
-	        int b = p&0xff;
+                int a = (p>>24)&0xff;
+                int r = (p>>16)&0xff;
+                int g = (p>>8)&0xff;
+                int b = p&0xff;
 
-	        //calculate average
-	        int avg = (r+g+b)/3;
+                // calculate average
+                int avg = (r+g+b)/3;
 
-	        //replace RGB value with avg
-	        p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+                //replace RGB value with avg
+                p = (a<<24) | (avg<<16) | (avg<<8) | avg;
 
-	        img.setRGB(x, y, p);
-	      }
-	    }
-	    return img;
-	}
-	
+                img.setRGB(x, y, p);
+            }
+        }
+        return img;
+    }
+
     @Override
     public void onHttpResponseReceive(HttpMessage msg) {
         if (msg.getResponseHeader().isEmpty() || !msg.getResponseHeader().isImage() || msg.getResponseBody().length() < 100 * 1024)
-			return;
+            return;
 
         try {
-            byte[] byteImage = msg.getResponseBody().getBytes();
-            InputStream in = new ByteArrayInputStream(byteImage);
-            BufferedImage bufferedImage = ImageIO.read(in);
-            if (bufferedImage == null) return;
-            BufferedImage resultImage = SizeBasedEnhancement.toGrayscale(bufferedImage);            
-           
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(resultImage, "png", baos);
-            baos.flush();
-            byte[] resultBytes = baos.toByteArray();
-			baos.close();
-	        msg.setResponseBody(resultBytes);
-	        msg.getResponseHeader().setContentLength(resultBytes.length);
-		} catch (IOException e) {
-        	Logger.getLogger(SizeBasedEnhancement.class).error(e.getMessage(), e);
-		}
+            byte[] inputBytes = msg.getResponseBody().getBytes();
+            BufferedImage input = imageFromBytes(inputBytes);
+            byte[] resultBytes = bytesFromImage(toGrayscale(input));
+            msg.setResponseBody(resultBytes);
+            msg.getResponseHeader().setContentLength(resultBytes.length);
+        } catch (IOException e) {
+            Logger.getLogger(SizeBasedEnhancement.class).error(e.getMessage(), e);
+        }
     }
 }
