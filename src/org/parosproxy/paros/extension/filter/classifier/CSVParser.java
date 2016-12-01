ab8634e6eb54c7e8182bd17ca11e3e4d3b0c1890
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2016-12-01 11:16:23 +0100

    Implement CSV parser

--- src/org/parosproxy/paros/extension/filter/classifier/CSVParser.java
+++ src/org/parosproxy/paros/extension/filter/classifier/CSVParser.java
@@ -1,13 +1,68 @@
 package org.parosproxy.paros.extension.filter.classifier;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CSVParser implements InappropriateTermParser {
 
+    private InappropriateTermFileContent fileContent;
+
     @Override
-    public List<Term> parseFileWithName(String fileName) {
-        // TODO: Implement this
-        return null;
+    public InappropriateTermFileContent parseFileWithName(String fileName) {
+
+        fileContent = new InappropriateTermFileContent();
+
+        try {
+            InputStream stream = getClass().getResourceAsStream(fileName);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                parseLine(line);
+            }
+        } catch (IOException e) {
+            // TODO: Handle this error
+        }
+
+        return fileContent;
+    }
+
+    private void parseLine(String line) {
+        if (line.startsWith("#")) return;
+
+        String[] parts = line.split(";");
+
+        if (parts.length == 3) {
+
+            parseTermLineFromParts(parts);
+
+        } else if (parts.length == 1) {
+
+            String[] thresholdParts = parts[0].split(" ");
+            parseThresholdLineFromParts(thresholdParts);
+        }
+    }
+
+    private void parseThresholdLineFromParts(String[] parts) {
+        if (parts.length != 2) return;
+        if (!parts[0].equals("threshold")) return;
+
+        fileContent.threshold = Integer.parseInt(parts[1]);
+    }
+
+    private void parseTermLineFromParts(String[] parts) {
+        String term = parts[0];
+        int score = Integer.parseInt(parts[1]);
+        Set<String> reasons = new HashSet<>(Arrays.asList(parts[2].split(",")));
+        fileContent.terms.add(new Term(term, score, reasons));
     }
 }
--- src/org/parosproxy/paros/extension/filter/classifier/InappropriateTermParser.java
+++ src/org/parosproxy/paros/extension/filter/classifier/InappropriateTermParser.java
@@ -1,6 +1,8 @@
 package org.parosproxy.paros.extension.filter.classifier;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -9,10 +11,14 @@ public interface InappropriateTermParser {
     InappropriateTermFileContent parseFileWithName(String fileName);
 
     class InappropriateTermFileContent {
-        public final int threshold;
-        public final List<Term> terms;
+        public int threshold;
+        public List<Term> terms;
 
-        public InappropriateTermFileContent(int threshold, List<Term> terms) {
+        InappropriateTermFileContent() {
+            this(0, new ArrayList<>());
+        }
+
+        InappropriateTermFileContent(int threshold, List<Term> terms) {
             this.threshold = threshold;
             this.terms = terms;
         }
@@ -23,7 +29,11 @@ public interface InappropriateTermParser {
         public final int score;
         public final Set<String> reasons;
 
-        public Term(String term, int score, Set<String> reasons) {
+        Term() {
+            this("", 0, new HashSet<>());
+        }
+
+        Term(String term, int score, Set<String> reasons) {
             this.term = term;
             this.score = score;
             this.reasons = reasons;
