6cbea40a54e2946782289ae45321ac64ea091e21
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2016-12-16 21:36:57 +0100

    Add image routine tests

--- test/org/parosproxy/paros/extension/filter/imageFilterActions/ImageFilterActionTest.java
+++ test/org/parosproxy/paros/extension/filter/imageFilterActions/ImageFilterActionTest.java
@@ -3,9 +3,17 @@ package org.parosproxy.paros.extension.filter.imageFilterActions;
 import org.junit.Test;
 import org.parosproxy.paros.network.HttpMessage;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.Assert.*;
 
 public class ImageFilterActionTest {
+
+    private static final Path BASE_DIR_TEST_FILES = Paths.get("test/resources/org/parosproxy/paros/extension/filter/imageFilterActions");
 
     @Test
     public void testLoadActionWithNameSuccessful() {
@@ -40,5 +48,43 @@ public class ImageFilterActionTest {
 
         action.setEnabled(false);
         assertFalse(action.isEnabled());
+    }
+
+    @Test
+    public void testImageConversions() throws IOException {
+        BufferedImage image = ImageIO.read(BASE_DIR_TEST_FILES.resolve("lena_color.gif").toFile());
+        byte[] bytes = ImageFilterAction.bytesFromImage(image);
+        assertNotNull(bytes);
+        assertTrue(bytes.length > 0);
+        assertTrue(compareImages(image, ImageFilterAction.imageFromBytes(bytes)));
+    }
+
+    /**
+     * Compares two images pixel by pixel.
+     *
+     * @param imgA the first image.
+     * @param imgB the second image.
+     * @return whether the images are both the same or not.
+     */
+    private static boolean compareImages(BufferedImage imgA, BufferedImage imgB) {
+        // The images must be the same size.
+        if (imgA.getWidth() == imgB.getWidth() && imgA.getHeight() == imgB.getHeight()) {
+            int width = imgA.getWidth();
+            int height = imgA.getHeight();
+
+            // Loop over every pixel.
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    // Compare the pixels for equality.
+                    if (imgA.getRGB(x, y) != imgB.getRGB(x, y)) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            return false;
+        }
+
+        return true;
     }
 }
