2135f7eceabb74779d8a79671af6978231b9706d
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2016-12-20 10:12:02 +0100

    Write Flip tests

---
+++ test/org/parosproxy/paros/extension/filter/FilterTestUtils.java
@@ -0,0 +1,87 @@
+package org.parosproxy.paros.extension.filter;
+
+import org.parosproxy.paros.network.HttpMessage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FilterTestUtils {
+
+    private static final String FORM_METHOD_TOKEN = "%%METHOD%%";
+    private static final String FORM_ACTION_TOKEN = "%%ACTION%%";
+    private static final String BASE_HTML_TOKEN = "%%BASE_HTML%%";
+
+    private static final Path BASE_DIR_HTML_FILES = Paths.get("test/resources/org/parosproxy/paros/extension/filter/html");
+
+    public static String readFile(Path file) throws IOException {
+        StringBuilder strBuilder = new StringBuilder();
+        for (String line : Files.readAllLines(file, StandardCharsets.UTF_8)) {
+            strBuilder.append(line).append('\n');
+        }
+        return strBuilder.toString();
+    }
+
+    public static HttpMessage createMessageWith(String filename) {
+        return createMessageWith(null, filename);
+    }
+
+    public static HttpMessage createMessageWith(String formMethod, String filename) {
+        return createMessageWith(formMethod, filename, null, null, "/");
+    }
+
+    public static HttpMessage createMessageWith(String formMethod, String filename, String formAction, String baseHtml) {
+        return createMessageWith(formMethod, filename, formAction, baseHtml, "/");
+    }
+
+    public static HttpMessage createMessageWith(
+            String formMethod,
+            String filename,
+            String formAction,
+            String baseHtml,
+            String requestUri) {
+        HttpMessage message = new HttpMessage();
+        try {
+            String fileContents = readFile(BASE_DIR_HTML_FILES.resolve(filename));
+            if (formMethod != null) {
+                fileContents = fileContents.replace(FORM_METHOD_TOKEN, formMethod);
+            }
+            if (formAction != null) {
+                fileContents = fileContents.replace(FORM_ACTION_TOKEN, formAction);
+            }
+            if (baseHtml != null) {
+                fileContents = fileContents.replace(BASE_HTML_TOKEN, baseHtml);
+            }
+            message.setRequestHeader("GET " + requestUri + " HTTP/1.1\r\nHost: example.com\r\n");
+            message.setResponseHeader(
+                    "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html; charset=UTF-8\r\n" + "Content-Length: "
+                            + fileContents.length());
+            message.setResponseBody(fileContents);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return message;
+    }
+
+    public static HttpMessage createMessageFromImage(String fileName, String imageType, String requestUri) {
+        HttpMessage message = new HttpMessage();
+        try {
+            BufferedImage img = ImageIO.read(BASE_DIR_HTML_FILES.resolve(fileName).toFile());
+            byte[] bytes = ((DataBufferByte)img.getRaster().getDataBuffer()).getData();
+            message.setRequestHeader("GET " + requestUri + " HTTP/1.1\r\nHost: example.com\r\n");
+            message.setResponseHeader(
+                    "HTTP/1.1 200 OK\r\n" + "Content-Type: image/" + imageType + "; charset=UTF-8\r\n" + "Content-Length: "
+                            + bytes.length);
+            message.setResponseBody(bytes);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return message;
+    }
+}
---
+++ test/org/parosproxy/paros/extension/filter/imageFilterActions/FlipTest.java
@@ -0,0 +1,43 @@
+package org.parosproxy.paros.extension.filter.imageFilterActions;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.parosproxy.paros.extension.filter.FilterTestUtils;
+import org.parosproxy.paros.network.HttpMessage;
+
+import static org.junit.Assert.*;
+
+public class FlipTest {
+
+    private Flip flip;
+
+    @Before
+    public void setup() {
+        flip = new Flip();
+    }
+
+    @Test
+    public void testDontFlipEmptyMessage() {
+        HttpMessage message = new HttpMessage();
+        HttpMessage original = new HttpMessage();
+        flip.onHttpResponseReceive(message);
+        assertEquals(message, original);
+    }
+
+    @Test
+    public void testDontFlipHTMLPage() {
+        HttpMessage message = FilterTestUtils.createMessageWith("simple.html");
+        HttpMessage original = FilterTestUtils.createMessageWith("simple.html");
+        flip.onHttpResponseReceive(message);
+        assertEquals(message, original);
+    }
+
+    @Test
+    public void testFlipImage() {
+        HttpMessage message = FilterTestUtils.createMessageFromImage("trump.jpg", "jpg", "www.trump.me");
+        HttpMessage original = FilterTestUtils.createMessageFromImage("trump.jpg", "jpg", "www.trump.me");
+        flip.onHttpResponseReceive(message);
+        assertFalse(message.getResponseBody().getBytes().equals(original.getResponseBody().getBytes()));
+        assertEquals(message.getResponseHeader().getContentLength(), message.getResponseBody().getBytes().length);
+    }
+}
